568f7b7a8b5db8189fa7e6156949e88f5180bc21
JeetRana1 <104169225+JeetRana1@users.noreply.github.com>
2026-02-06 19:14:17 -0500

    Fix: Restored missing syntax to enable Tor Passthrough for blocked audio streams

--- controllers/proxy.ts
+++ controllers/proxy.ts
@@ -24,7 +24,13 @@ export default async function proxy(req: Request, res: Response) {
                 responseType: 'stream',
                 headers: {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-                    "Accept": "*/*"
+                    "Accept": "*/*",
+                    "Accept-Language": "en-US,en;q=0.9",
+                    "Sec-Fetch-Dest": "empty",
+                    "Sec-Fetch-Mode": "cors",
+                    "Sec-Fetch-Site": "cross-site",
+                    "Pragma": "no-cache",
+                    "Cache-Control": "no-cache"
                 },
                 httpAgent: torAgent,
                 httpsAgent: torAgent,
